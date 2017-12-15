d7a56a69137088282ae99ecbdf1b81fb7e7b262c
Chris Doggett <CDoggett@covermymeds.com>
2017-12-15 10:42:08 -0600

    Renamed Day10 TwistList to KnotHash

--- lib/day10.ex
+++ lib/day10.ex
@@ -1,15 +1,15 @@
-defmodule TwistList do
+defmodule KnotHash do
   use Bitwise
 
   defstruct list: [], current: 0, list_length: 256, skip_size: 0
 
-  def build(list_length \\ 256), do: %TwistList{ list: Enum.to_list(0..(list_length-1)), list_length: list_length }
+  def build(list_length \\ 256), do: %KnotHash{ list: Enum.to_list(0..(list_length-1)), list_length: list_length }
 
-  def twist_lengths(%TwistList{} = twist_list, lengths) do
-    Enum.reduce(lengths, twist_list, fn size, acc -> twist(acc, size) end)
+  def twist_lengths(%KnotHash{} = knot_hash, lengths) do
+    Enum.reduce(lengths, knot_hash, fn size, acc -> twist(acc, size) end)
   end
 
-  def twist(%TwistList{ list: list, current: current, list_length: list_length, skip_size: skip_size }, twist_length) do
+  def twist(%KnotHash{ list: list, current: current, list_length: list_length, skip_size: skip_size }, twist_length) do
     new_list = list
                |> Stream.cycle()
                |> Stream.drop(current)
@@ -19,7 +19,7 @@ defmodule TwistList do
                |> Stream.drop(list_length - current)
                |> Enum.take(list_length)
 
-    %TwistList{
+    %KnotHash{
       list: new_list,
       current: rem(current + twist_length + skip_size, list_length),
       list_length: list_length,
@@ -30,8 +30,8 @@ defmodule TwistList do
   defp do_twist(list, stack, 0), do: stack ++ list
   defp do_twist([h | t], stack, skip), do: do_twist(t, [h | stack], skip - 1)
 
-  def hash(%TwistList{} = twist_list, lengths) do
-    %TwistList{ list: sparse_hash } = do_hash_rounds(twist_list, lengths, 64)
+  def hash(%KnotHash{} = knot_hash, lengths) do
+    %KnotHash{ list: sparse_hash } = do_hash_rounds(knot_hash, lengths, 64)
 
     sparse_hash
     |> Enum.chunk(16)
@@ -40,9 +40,9 @@ defmodule TwistList do
     |> String.downcase()
   end
 
-  defp do_hash_rounds(%TwistList{} = twist_list, _lengths, 0), do: twist_list
-  defp do_hash_rounds(%TwistList{} = twist_list, lengths, hash_round) do
-    do_hash_rounds(TwistList.twist_lengths(twist_list, lengths), lengths, hash_round - 1)
+  defp do_hash_rounds(%KnotHash{} = knot_hash, _lengths, 0), do: knot_hash
+  defp do_hash_rounds(%KnotHash{} = knot_hash, lengths, hash_round) do
+    do_hash_rounds(KnotHash.twist_lengths(knot_hash, lengths), lengths, hash_round - 1)
   end
 
   defp calc_dense_hash_element(list) do
@@ -56,8 +56,8 @@ end
 defmodule Day10 do
   def part1(lengths, list_length \\ 256) do
     list_length
-    |> TwistList.build()
-    |> TwistList.twist_lengths(lengths)
+    |> KnotHash.build()
+    |> KnotHash.twist_lengths(lengths)
     |> Map.get(:list)
     |> Enum.take(2)
     |> Enum.reduce(1, &Kernel.*/2)
@@ -66,6 +66,9 @@ defmodule Day10 do
   @length_padding <<17, 31, 73, 47, 23>>
 
   def part2(input) do
-    TwistList.hash(TwistList.build(), to_charlist(input <> @length_padding))
+    padded_input = input <> @length_padding |> to_charlist
+
+    KnotHash.build()
+    |> KnotHash.hash(padded_input)
   end
 end
