f8ccda27d7f5c6d7ef9e1dd17b2c61fb061ea973
Chris Doggett <CDoggett@covermymeds.com>
2017-12-14 18:20:49 -0600

    Finished day 13

---
+++ lib/day13.ex
@@ -0,0 +1,80 @@
+defmodule Firewall do
+  defstruct layers: []
+
+  defmodule Layer do
+    defstruct depth: 0, range: 0, stream: nil
+
+    def new(depth, range) do
+      %Layer{
+        depth: depth,
+        range: range,
+        stream: build_stream(range)
+        # fast-forward to where scanner will be at time(depth)
+        # to simplify math when checking delays
+      }
+    end
+
+    defp build_stream(n) do
+      [h | t] = Enum.to_list(0..(n-1))
+      [_ | t2] = Enum.reverse(t)
+
+      [h | t] ++ t2
+    end
+
+    def position_at(%Layer{stream: stream, depth: depth, range: range}, delay) do
+      pos = rem(depth + delay, 2*(range-1))
+
+      Enum.at(stream, pos)
+    end
+  end
+
+  def new(input) do
+    %Firewall{
+      layers: input
+      |> String.strip()
+      |> String.split("\n")
+      |> Enum.map(&parse_line/1)
+    }
+  end
+
+  def penalty_at(%Firewall{ layers: layers }, delay) do
+    layers
+    |> Enum.map(&{&1.depth * &1.range, Layer.position_at(&1, delay) })
+    |> Enum.map(fn {score, 0} -> score; _ -> 0 end)
+    |> Enum.sum()
+  end
+
+  def straight_shot?(%Firewall{ layers: layers }, delay) do
+    layers
+    |> Enum.map(&(Layer.position_at(&1, delay)))
+    |> Enum.all?(fn 0 -> false; _ -> true end)
+  end
+
+  defp parse_line(line) do
+    [depth, range] = line
+                     |> String.strip()
+                     |> String.split(": ")
+                     |> Enum.map(&String.to_integer/1)
+
+    Firewall.Layer.new(depth, range)
+  end
+end
+
+defmodule Day13 do
+  def part1(input) do
+    input
+    |> Firewall.new()
+    |> Firewall.penalty_at(0)
+  end
+
+  def part2(input) do
+    firewall = Firewall.new(input)
+
+    1..10_000_000
+    |> Stream.map(&{&1, Firewall.straight_shot?(firewall, &1)})
+    |> Stream.drop_while(fn {_, open} -> !open end)
+    |> Enum.take(1)
+    |> hd
+    |> elem(0)
+  end
+end
---
+++ test/day13_test.exs
@@ -0,0 +1,31 @@
+defmodule Day13Test do
+  use ExUnit.Case
+  doctest Day13
+
+  test "Day 13, Part 1" do
+    assert Day13.part1(~S{
+      0: 3
+      1: 2
+      4: 4
+      6: 4}) == 24
+
+    actual = "test/fixtures/day13_input"
+             |> File.read!()
+             |> Day13.part1()
+    assert actual == 1316
+  end
+
+  test "Day 13, Part 2" do
+    assert Day13.part2(~S{
+      0: 3
+      1: 2
+      4: 4
+      6: 4}) == 10
+
+    # Test on actual input takes ~33s
+     actual = "test/fixtures/day13_input"
+              |> File.read!()
+              |> Day13.part2()
+     assert actual == 3840052
+  end
+end
